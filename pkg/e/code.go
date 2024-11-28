90debc6f1f632f433eca217f9a8a7ed68c5fe2ee
baoyoupeng <310757801@qq.com>
2024-11-28 14:00:57 +0800

    增加自定义返回

---
+++ pkg/e/code.go
@@ -0,0 +1,32 @@
+package e
+
+// 全局
+const (
+	SUCCESS int = 200
+	InvalidParams int = 400
+	ERROR int = 500
+)
+
+// 用户
+const (
+	ErrorExistUser int = 10001 + iota
+	ErrorNotExistUser
+
+)
+
+var CodeMsg = map[int]string {
+	SUCCESS:          "请求成功",
+	InvalidParams:  "请求参数错误",
+	ERROR:           "请求失败",
+
+	ErrorExistUser: "用户已存在",
+	ErrorNotExistUser: "用户不存在",
+
+}
+
+func GetMsg(code int) string {
+	if msg, ok := CodeMsg[code]; ok {
+		return msg
+	}
+	return CodeMsg[ERROR]
+}
